8c4370a2a48a096649e08022080d39f94c98b6a0
choinara0 <chlskfk6810@naver.com>
2021-08-24 15:01:11 +0900

    connect mongoDB

--- server/config/dev.js
+++ server/config/dev.js
@@ -1,3 +1,3 @@
 module.exports = {
-    mongoURI:''
+    mongoURI:'mongodb+srv://choinara0:<password>@cluster0.pblm5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority' //mongoDB를 사용할 때, mongoDB와 app을 연결시키는 key값
 }

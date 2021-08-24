fd6ff8c77b1bc9c6c5cee5d2029e33dee6457adc
choinara0 <chlskfk6810@naver.com>
2021-08-24 17:46:28 +0900

    connect mongoDB

--- server/config/dev.js
+++ server/config/dev.js
@@ -1,3 +1,3 @@
 module.exports = {
-    mongoURI:'mongodb+srv://choinara0:<password>@cluster0.pblm5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority' //mongoDB를 사용할 때, mongoDB와 app을 연결시키는 key값
+    mongoURI:'mongodb+srv://choinara0:zxc123123^^@cluster0.pblm5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority' //mongoDB를 사용할 때, mongoDB와 app을 연결시키는 key값
 }

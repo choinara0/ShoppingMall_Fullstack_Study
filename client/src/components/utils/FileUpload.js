8a97d315044b19398dc6076b086b0a7fda2e1340
choinara0 <chlskfk6810@naver.com>
2021-08-26 15:28:32 +0900

    DropZone UI 설정

--- client/src/components/utils/FileUpload.js
+++ client/src/components/utils/FileUpload.js
@@ -1,11 +1,25 @@
 import React from 'react';
+import Dropzone from 'react-dropzone'
+import {Icon} from 'antd'
 
-function FileUpload(props) {
+function FileUpload() {
     return (
-        <div>
-
-            file upload
-
+        <div style={{display: 'flex', justifyContent: 'space-between'}}>
+            <Dropzone onDrop={acceptedFiles => console.log(acceptedFiles)}>
+                {({getRootProps, getInputProps}) => (
+                    <section>
+                        <div
+                            style={{
+                                width: '300px', height: '240px', border:'1px solid lightgray',
+                                display: 'flex', alignItems: 'center', justifyContent: 'center'
+                            }}
+                            {...getRootProps()}>
+                            <input {...getInputProps()} />
+                            <Icon type='plus' style={{fontSize: '3rem'}}/>
+                        </div>
+                    </section>
+                )}
+            </Dropzone>
         </div>
     );
 }

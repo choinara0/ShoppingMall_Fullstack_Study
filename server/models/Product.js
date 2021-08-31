f689fbae5d247d0cc9346504f9a1639f975f77eb
choinara0 <chlskfk6810@naver.com>
2021-08-31 23:55:39 +0900

    ProductSchema 설정

--- server/models/Product.js
+++ server/models/Product.js
@@ -1,8 +1,37 @@
 const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
 const productSchema = mongoose.Schema({
+    writer: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    title: {
+        type: String,
+        maxlength: 50
+    },
+    description: {
+        type: String,
+    },
+    price: {
+        type: Number,
+        default: 0
+    },
+    images: {
+        type: Array,
+        default: []
+    },
+    sold: {
+        type: Number,
+        maxlength: 100,
+        default: 0
+    },
+    views: {
+        type: Number,
+        default: 0
+    }
 
-})
+}, {timestamps: true})  //timestamps를 설정하면 등록시간이 자동으로 업뎃됨
 
 
 const Product = mongoose.model('User', productSchema);

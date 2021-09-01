99b0203cf18e24305b36f7a468ea47f5ef1fa9d8
choinara0 <chlskfk6810@naver.com>
2021-09-01 17:06:58 +0900

    rending page 기본 UI 설정을 위한 setting

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,24 +1,55 @@
-import React, {useEffect} from 'react'
+import React, {useEffect, useState} from 'react'
 import { FaCode } from "react-icons/fa";
 import axios from 'axios'
+import {Icon, Col, Card, Row} from 'antd';
+import Meta from "antd/lib/card/Meta";
+
 
 function LandingPage() {
-
+    const [Products, setProducts] = useState([])
 
     useEffect(()=>{
         axios.post('api/product/products')
             .then(response=>{
                 if(response.data.success){
                     console.log(response.data)
+                    setProducts(response.data.productInfo)
                 }else{
                     alert('상품을 가져오는데 실패했습니다.')
                 }
             })
     }, [])
 
+    const renderCards = Products.map((product, index) => {
+        console.log(product)
+        return <Card
+            key={index}
+            cover={<img src={`http://localhost:5000/${product.images[0]}`}/>}
+        >
+            <Meta
+            title={product.title}
+            description={`$${product.price}`}
+
+            />
+        </Card>
+    })
+
     return (
-        <div>
-            landing Page
+        <div style={{width: '75%', margin: '3rem auto'}}>
+            <div style={{textAlign: 'center'}}>
+                <h2>Let's travel anywhere <Icon type="rocket"/></h2>
+            </div>
+
+            {/*{filter}*/}
+            {/*{search}*/}
+            {/*{Card}*/}
+
+            {renderCards}
+
+            <div style={{justifyContent: 'center'}}>
+                <button> 더 보기 </button>
+            </div>
+
         </div>
     )
 }

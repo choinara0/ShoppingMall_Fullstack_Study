e8a169fce11bd4c0b76974fb5b789f48433074ab
choinara0 <chlskfk6810@naver.com>
2021-09-05 17:06:46 +0900

    Checkbox 선택한 것들은 Checked에 업데이트

--- client/src/components/views/LandingPage/Sections/CheckBox.js
+++ client/src/components/views/LandingPage/Sections/CheckBox.js
@@ -1,13 +1,31 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Checkbox, Collapse} from "antd";
 
 const {Panel} = Collapse;
 
 function CheckBox(props) {
+    const [Checked, setChecked] = useState([])
+    const handleToggle = (value) => {
+        // 누른 것의 Index를 구하고
+
+        const currentIndex = Checked.indexOf(value)
+
+
+
+        const newChecked = [...Checked] //spread syntax를 이용하여 전체 checked를 가져온다.
+        if(currentIndex === -1){ //전체 Checked된 State에서 현재 누른 Checkbox가 없다면 넣어주고
+            newChecked.push(value)
+        }else{// 전체 Checked된 State에서 현재 누른 Checkbox가 있다면 빼주고
+            newChecked.splice(currentIndex, 1)
+        }
+
+        setChecked(newChecked)
+    }
 
     const renderCheckBoxLists = () => props.list&& props.list.map((value, index) => (
         <React.Fragment key={index}>
-            <Checkbox onChange/>
+            <Checkbox onChange={() => handleToggle(value._id)}
+                      checked={Checked.indexOf(value._id) === -1 ? false : true}/>
                 <span>{value.name}</span>
         </React.Fragment>
     ))

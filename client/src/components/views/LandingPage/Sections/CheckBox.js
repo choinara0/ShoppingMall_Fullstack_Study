222c4709cc361a494a6cddb9306ba33dc4aef15b
choinara0 <chlskfk6810@naver.com>
2021-09-05 16:48:31 +0900

    props를 받아서 checkbox의 value.name 가져오기

--- client/src/components/views/LandingPage/Sections/CheckBox.js
+++ client/src/components/views/LandingPage/Sections/CheckBox.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import {Checkbox, Collapse} from "antd";
+
+const {Panel} = Collapse;
 
 function CheckBox(props) {
+
+    const renderCheckBoxLists = () => props.list&& props.list.map((value, index) => (
+        <React.Fragment key={index}>
+            <Checkbox onChange>
+                <span>{value.name}</span>
+            </Checkbox>
+        </React.Fragment>
+    ))
+
     return (
         <div>
-            CheckBox
+            <Collapse defaultActiveKey={['1']}>
+                <Panel header="This is panel header 1" key="1">
+                    {renderCheckBoxLists()}
+                </Panel>
+            </Collapse>,
         </div>
     );
 }

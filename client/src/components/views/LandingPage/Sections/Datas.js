f06f595bb22c4cb3f6372396584d84130971ba49
choinara0 <chlskfk6810@naver.com>
2021-09-05 16:31:58 +0900

    filter에 담을 datas 생성

---
+++ client/src/components/views/LandingPage/Sections/Datas.js
@@ -0,0 +1,24 @@
+const cotinents = [
+    {
+        "_id": 1,
+        "name": 'Africa'
+    },{
+        "_id": 2,
+        "name": 'Asia'
+    },{
+        "_id": 3,
+        "name": 'Australia'
+    },{
+        "_id": 4,
+        "name": 'Antarctica'
+    },{
+        "_id": 5,
+        "name": 'Europe'
+    },{
+        "_id": 6,
+        "name": 'North America'
+    },{
+        "_id": 7,
+        "name": 'South America'
+    },
+]

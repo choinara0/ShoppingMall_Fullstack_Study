09fb9f692b1fa469c1da247d83b335282a45637c
choinara0 <chlskfk6810@naver.com>
2021-09-06 19:02:34 +0900

    import searchFeature

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -6,6 +6,7 @@ import ImageSlider from "../../utils/ImageSlider";
 import CheckBox from "./Sections/CheckBox";
 import RadioBox from "./Sections/RadioBox";
 import {continents, price} from "./Sections/Datas";
+import SearchFeature from "./Sections/SearchFeature";
 
 
 
@@ -120,6 +121,7 @@ function LandingPage() {
             </Row>
 
             {/*{search}*/}
+            <SearchFeature></SearchFeature>
             {/*{Card}*/}
             <Row gutter={[16, 16]}> {/*카드끼리의 여백 설정*/}
                 {renderCards}

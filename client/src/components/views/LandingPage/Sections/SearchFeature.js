b2c26acdb0f2b9decb9b2de29147fb34ac0006a2
choinara0 <chlskfk6810@naver.com>
2021-09-06 19:12:02 +0900

    onChange를 이용해서 타이핑한 것을 SearchTerm에 담고, 업데이트

--- client/src/components/views/LandingPage/Sections/SearchFeature.js
+++ client/src/components/views/LandingPage/Sections/SearchFeature.js
@@ -1,14 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Input} from "antd";
 
 const {Search} = Input;
 
 function SearchFeature(props) {
+
+    const [SearchTerm, setSearchTerm] = useState("")
+
+    const searchHandler = (event) => {
+        setSearchTerm(event.target.value)
+    }
+
     return (
         <div>
             <Search
                 placeholder="input search text"
+                onChange={searchHandler}
                 style={{width: 200}}
+                value={SearchTerm}
             />
         </div>
     );
